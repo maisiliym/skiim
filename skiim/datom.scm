9224c1bcdeb13d89c1e058d1299f1186b02103ec
li <@li:maisiliym.uniks>
2021-04-20 02:41:05 +0000

    (init (draft datom))

---
+++ skiim/datom.scm
@@ -0,0 +1,8 @@
+(define-module (skiim datom)
+  #:use-module (oop goops)
+  #:export ())
+
+(define-syntax spici
+ (syntax-rules ()
+  ((spici spiciNeim metaSpiciz inyrDatom inyrDatom* ...)
+   (define-class spiciNeim ) ) ))
