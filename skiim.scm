4db8e13f33caa1712be5795070cef5c059936d1e
li <li@maisiliym.uniks>
2021-06-16 12:53:25 +0700

    (snap debugMacroMadness)

--- skiim.scm
+++ skiim.scm
@@ -7,31 +7,40 @@
     (syntax-case x ()
       ((_ spici sob-datom ...)
        (with-syntax
-	   (((sob-datom-def ...)
-	     (meik-sob-datoms #'(sob-datom ...))))
+	   ((sob-datom-def ...)
+	     (meik-sob-datoms #'(sob-datom ...)))
 	 #'(define-class spici () . (sob-datom-def ...)))))))
 
-(define (meik-sob-datoms x)
-  (syntax-case x ()
-    ((sob-datom)
-     (meik-sob-datom #'sob-datom))
-    ((sob-datom sob-datom* ...)
-     (with-syntax
-	 ((sob-datom-def (meik-sob-datom #'sob-datom))
-	  ((sob-datom-def* ...) (meik-sob-datoms #'(sob-datoms* ...))))
-       #'(sob-datom-def . (sob-datom-def* ...))))))
+(define-syntax meik-sob-datoms
+  (lambda (x)
+    (syntax-case x ()
+      ((_ sob-datom)
+       #'(meik-sob-datom sob-datom))
+      ((_ sob-datom sob-datom* ...)
+       (with-syntax
+	   ((sob-datom-def (meik-sob-datom #'sob-datom))
+	    ((sob-datom-def* ...) (meik-sob-datoms #'(sob-datom* ...))))
+	 #'(sob-datom-def . (sob-datom-def* ...)))))))
 
-(define (meik-sob-datom x)
-  (syntax-case x ()
-    ((neim spici)
-     (with-syntax
-	 (let ((neim-symbol (syntax->datum #'neim)))
-	   ((init-symbol (symbol-append '#: neim-symbol))
-	    (get-symbol (symbol-append '-> neim-symbol))
-	    (set-symbol (symbol-append '<- neim-symbol))))
-       #'(neim #:init-keyword init-symbol
-	       #:getter get-symbol
-	       #:setter set-symbol)))))
+(define (id-append ctx a b)
+  (datum->syntax ctx (symbol-append (syntax->datum a) (syntax->datum b))))
+
+(define-syntax meik-sob-datom
+  (lambda (x)
+    (syntax-case x ()
+      ((_ (neim spici))
+       (with-syntax
+	   (
+	    (init-prefix "#:")
+	    (get-prefix "->")
+	    (set-prefix "<-")
+	    (init-symbol (id-append x '#: '#neim))
+	    (get-symbol (id-append x '-> '#neim))
+	    (set-symbol (id-append x '<- '#neim)))
+	 #`(neim
+	    #:init-keyword #,(id-append '#neim #'init-prefix '#neim)
+	    #:getter #,(id-append '#neim #'get-prefix '#neim)
+	    #:setter #,(id-append '#neim #'set-prefix '#neim)))))))
 
 ;; test
 (define-datom <prikriom>
