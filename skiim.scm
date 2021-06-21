73cd1bfaeb6a08bdaf601c8a9c7e8ce87de11a67
li <li@maisiliym.uniks>
2021-06-21 15:27:13 +0700

    (snap someProgress)

--- skiim.scm
+++ skiim.scm
@@ -4,44 +4,35 @@
 
 (define-syntax define-datom
   (lambda (x)
+    (define (meik-sob-datom sd)
+      (define (id-append ctx a b)
+	(datum->syntax ctx (symbol-append (string->symbol a) (syntax->datum b))))
+      (syntax-case sd ()
+	((neim spici)
+	 (with-syntax
+	     ((init-symbol (id-append #'neim "#:" #'neim))
+	      (get-symbol (id-append #'neim "->" #'neim))
+	      (set-symbol (id-append #'neim "<-" #'neim)))
+	   #`(neim
+	      #:init-keyword init-symbol
+	      #:getter get-symbol
+	      #:setter set-symbol)))))    
+    (define (meik-sob-datoms sds)
+      (syntax-case sds ()
+	(() #'())
+	((sob-datom)
+	 (meik-sob-datom #'sob-datom))
+	((sob-datom sob-datom* ...)
+	 (with-syntax
+	     ((sob-datom-def (meik-sob-datom #'sob-datom))
+	      ((sob-datom-def* ...) (meik-sob-datoms #'(sob-datom* ...))))
+	   #'(sob-datom-def sob-datom-def* ...)))))
     (syntax-case x ()
       ((_ spici sob-datom sob-datom* ...)
        (with-syntax
 	   ((sob-datom-def (meik-sob-datom #'sob-datom))
-	    (sob-datom-def* ...)
-	    (meik-sob-datoms #'(sob-datom* ...)))
-	 #'(define-class spici () sob-datom-def ...))))))
-
-(define (meik-sob-datoms x)
-  (syntax-case x ()
-    ((sob-datom)
-     #'(meik-sob-datom sob-datom))
-    ((sob-datom sob-datom* ...)
-     (with-syntax
-	 ((sob-datom-def (meik-sob-datom #'sob-datom))
-	  ((sob-datom-def* ...) (meik-sob-datoms #'(sob-datom* ...))))
-       #'(sob-datom-def sob-datom-def* ...)))))
-
-(define (id-append ctx a b)
-  (datum->syntax ctx (symbol-append (syntax->datum a) (syntax->datum b))))
-
-(define (meik-sob-datom x)
-  (define (id-append ctx a b)
-    (datum->syntax ctx (symbol-append (syntax->datum a) (syntax->datum b))))
-  (syntax-case x ()
-    ((neim spici)
-     (with-syntax
-	 ((init-prefix "#:")
-	  (get-prefix "->")
-	  (set-prefix "<-")
-	  ;; (init-symbol (id-append x '#: '#neim))
-	  ;; (get-symbol (id-append x '-> '#neim))
-	  ;; (set-symbol (id-append x '<- '#neim))
-	  )
-       #`(neim
-	  #:init-keyword #,(id-append neim #'init-prefix '#neim)
-	  #:getter #,(id-append neim #'get-prefix '#neim)
-	  #:setter #,(id-append neim #'set-prefix '#neim))))))
+	    (sob-datom-defs (meik-sob-datoms #'(sob-datom* ...))))
+	 #'(define-class spici () sob-datom-def sob-datom-defs))))))
 
 ;; test
 (define-datom <prikriom>
