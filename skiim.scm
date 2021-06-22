51ac543d8e5928ad20926db0c725c3e8465b85a2
li <li@maisiliym.uniks>
2021-06-22 13:25:03 +0700

    (snap)

--- skiim.scm
+++ skiim.scm
@@ -11,17 +11,23 @@
       (syntax-case sd ()
 	((neim spici)
 	 (with-syntax
-	     ((init-symbol (id-append #'neim "#:" #'neim))
+	     ((init-symbol (id-append #'neim ":" #'neim))
 	      (get-symbol (id-append #'neim "->" #'neim))
 	      (set-symbol (id-append #'neim "<-" #'neim)))
-	   #`(neim #:init-keyword init-symbol
+	   #'(neim #:init-keyword init-symbol
 		   #:getter get-symbol
 		   #:setter set-symbol)))))
     (define (meik-sob-datoms sds)
       (syntax-case sds ()
-	(() #'())
 	((sob-datom)
 	 (meik-sob-datom #'sob-datom))
+	((sob-datom sob-datom*)
+	 (with-syntax
+	     (((sob-datom-def sob-datom-def*)
+	       (map (lambda (sd)
+		      (meik-sob-datom sd))
+		    #'(sob-datom sob-datom*))))
+	   #'(sob-datom-def sob-datom-def*)))
 	((sob-datom sob-datom* ...)
 	 (with-syntax
 	     ((sob-datom-def (meik-sob-datom #'sob-datom))
@@ -29,9 +35,19 @@
 	   #'(sob-datom-def sob-datom-def* ...)))))
     (syntax-case x ()
       ((_ spici sob-datom ...)
-       #`(define-class spici () #,(meik-sob-datoms #'(sob-datom ...)))))))
+       (with-syntax
+	   ((sob-datom-defs (meik-sob-datoms #'(sob-datom ...))))
+	 #`(define-class spici () sob-datom-defs))))))
 
 ;; test
+;; test
+(define-datom <mono-datom>
+  (sob-dat <string>))
+
+(define-datom <duo-datom>
+  (one-dat <string>)
+  (two-dat <string>))
+
 (define-datom <prikriom>
   (ful <string>)
   (ssh <string>)
